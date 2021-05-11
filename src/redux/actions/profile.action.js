68cb13114846d0a952c7ab550dfa24a3905779f9
Phumai <mcc3939@gmail.com>
2021-05-11 14:45:54 +0700

    fix

--- src/redux/actions/profile.action.js
+++ src/redux/actions/profile.action.js
@@ -1,9 +1,9 @@
 import { PROFILE_DATA, TOKEN_DATA } from "./type";
 
-export const updateToken = (payload) => async (dispatch) => {
+export const updateToken = (payload) => (dispatch) => {
   dispatch({ type: TOKEN_DATA, payload: payload });
 };
 
-export const updateProfile = (payload) => async (dispatch) => {
+export const updateProfile = (payload) => (dispatch) => {
   dispatch({ type: PROFILE_DATA, payload: payload });
 };
